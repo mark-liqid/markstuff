18366976f885b82fe31593abb3d605ab5b37c25e
Mark <mark.mckeen@liqid.com>
2021-02-26 16:10:19 -0700

    New script for running nvidia docker and tf benchmark

--- mlhelper/runnvidia.py
+++ mlhelper/runnvidia.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 import sys
 
@@ -6,11 +7,13 @@ batch_size = int(raw_input('Batch size? '))
 num_gpu = int(raw_input('Number of GPUs? '))
 
 iterations = int(raw_input('How many iterations do you want to run? '))
-logging = str(raw_input('Do you want to log this run? y/n'))
-fp = int(raw_input('Do you want to run fp32? Default is fp16. y/n'))
-#print("Running the benchmark with the following values: ")
-#print('Batch size: ',batch_size,'Number of GPUS: ',num_gpu,'Iterations: ',iterations,'Prescision :',fp,'With XLA?',xla,')
+logging = str(raw_input('Do you want to log this run? y/n '))
+fp = str(raw_input('Do you want to run fp32? Default is fp16. y/d '))
 
+if fp == 'y':
+    fp = str('fp16')
+elif fp =='n':
+    fp = str('fp32')
 #requesting root for docker run
 euid = os.geteuid()
 if euid != 0:
@@ -18,4 +21,4 @@ if euid != 0:
     args = ['sudo', sys.executable] + sys.argv + [os.environ]
     os.execlpe('sudo', *args)
     pass
-os.system("docker run --runtime=nvidia --rm nvcr.io/nvidia/tensorflow:20.12-tf2-py3 python /benchmarks_master/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --data_format=NCHW --batch_size=",batch_size," --num_batches=",iterations," --model=resnet50 --optimizer=momentum --variable_update=replicated --all_reduce_spec=nccl --nodistortions --gradient_repacking=2 --datasets_use_prefetch=True --per_gpu_thread_count=2 --loss_type_to_report=base_loss --compute_lr_on_cpu=True --single_l2_loss_op=True --xla_compile=True --local_parameter_device=gpu --num_gpus=",num_gpu," --display_every=10")
+subprocess.Popen("docker run --runtime=nvidia --rm nvcr.io/nvidia/tensorflow:20.12-tf2-py3 python /benchmarks_master/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --data_format=NCHW --batch_size=",batch_size," --num_batches=",iterations," --model=resnet50 --optimizer=momentum --variable_update=replicated --all_reduce_spec=nccl --nodistortions --gradient_repacking=2 --datasets_use_prefetch=True --per_gpu_thread_count=2 --loss_type_to_report=base_loss --compute_lr_on_cpu=True --single_l2_loss_op=True --xla_compile=True --local_parameter_device=gpu --num_gpus=",num_gpu," --display_every=10")
